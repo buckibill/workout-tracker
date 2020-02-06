aa902096715185852d3992836a892721d2fdf21c
Bill Murray <buckibill@gmail.com>
2020-02-06 15:30:03 -0800

    added files and code

---
+++ public/api.js
@@ -0,0 +1,40 @@
+const API = {
+  async getLastWorkout() {
+    const res = await fetch("/api/workouts");
+    const json = await res.json();
+
+    return json[json.length - 1];
+  },
+  async addExercise(data) {
+    const id = location.search.split("=")[1];
+    console.log(data)
+
+    const res = await fetch("/api/workouts/" + id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const json = await res.json();
+
+    return json;
+  },
+  async createWorkOut() {
+    const res = await fetch("/api/workouts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" }
+    });
+
+    const json = await res.json();
+    console.log(json)
+
+    return json;
+  },
+
+  async getWorkoutsInRange() {
+    const res = await fetch(`/api/workouts/range`);
+    const json = await res.json();
+    
+    return json;
+  },
+};
